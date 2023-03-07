33e7c981503e03578af21be627249aae9143a34e
Alexthw46 <77152778+Alexthw46@users.noreply.github.com>
2023-03-07 10:43:15 +0100

    fix data augmentation, move from code package

---
+++ code_py/preprocessing.py
@@ -0,0 +1,106 @@
+import os
+import random
+
+import numpy as np
+import albumentations as abm
+from PIL import Image
+from keras.utils import load_img, Sequence
+
+
+class DataGen(Sequence):
+    """Helper to iterate over the data (as Numpy arrays)."""
+
+    def __init__(self, batch_size, img_size, input_img_paths, target_img_paths, aug):
+        self.batch_size = batch_size
+        self.img_size = img_size
+        self.input_img_paths = input_img_paths
+        self.target_img_paths = target_img_paths
+        self.aug = aug
+
+    def __len__(self):
+        return len(self.target_img_paths) // self.batch_size
+
+    def __getitem__(self, idx):
+        augmentation = abm.Compose([
+            abm.RandomRotate90(p=0.3),
+            abm.HorizontalFlip(p=0.3),
+            abm.VerticalFlip(p=0.3)
+        ])
+        """Returns tuple (input, target) correspond to batch #idx."""
+        i = idx * self.batch_size
+        batch_input_img_paths = self.input_img_paths[i: i + self.batch_size]
+        batch_target_img_paths = self.target_img_paths[i: i + self.batch_size]
+        x = np.zeros((self.batch_size,) + self.img_size + (4,), dtype="float32")
+        for j, path in enumerate(batch_input_img_paths):
+            img = load_img(path, target_size=self.img_size, color_mode='rgba')
+            x[j] = img
+        y = np.zeros((self.batch_size,) + self.img_size + (1,), dtype="uint8")
+        for j, path in enumerate(batch_target_img_paths):
+            img = load_img(path, target_size=self.img_size, color_mode="grayscale")
+            y[j] = np.expand_dims(img, 2)
+            # Ground truth labels are 1, 2, 3, 4. Subtract to get 0, 1, 2, 3:
+            y[j] -= 1
+            if self.aug:
+                transformed = augmentation(image=x[j], mask=y[j])
+                x[j], y[j] = transformed['image'], transformed['mask']
+        return x, y
+
+
+def makeGens(input_img_paths, target_img_paths, batch_size, img_size, val_split=0.2, aug=False):
+    # Split our img paths into a training and a validation set
+    val_samples = int(len(input_img_paths) * val_split)
+    random.Random(1337).shuffle(input_img_paths)
+    random.Random(1337).shuffle(target_img_paths)
+    train_input_img_paths = input_img_paths[:-val_samples]
+    train_target_img_paths = target_img_paths[:-val_samples]
+    val_input_img_paths = input_img_paths[-val_samples:]
+    val_target_img_paths = target_img_paths[-val_samples:]
+
+    val_paths = (val_input_img_paths, val_target_img_paths)
+
+    # Instantiate data Sequences for each split
+    train_gen = DataGen(
+        batch_size, img_size, train_input_img_paths, train_target_img_paths, aug
+    )
+    val_gen = DataGen(batch_size, img_size, val_input_img_paths, val_target_img_paths, aug)
+
+    return train_gen, val_gen, val_paths
+
+
+def pregenDataAugm(input_img_paths, target_img_paths, in_path, tg_path):
+    augmentation = abm.Compose([
+        abm.RandomRotate90(p=0.3),
+        abm.HorizontalFlip(p=0.3),
+        abm.VerticalFlip(p=0.3)
+    ])
+
+    for input, target in input_img_paths, target_img_paths:
+        image = Image.open(os.path.join(in_path, input))
+        mask = Image.open(os.path.join(in_path, target))
+
+        augmentation(image=image, mask=mask)
+
+
+# convert the rgba image to a grayscale map [(0, background), (1, walls), (2, rooms), (3, lights)]
+# noinspection PyUnresolvedReferences
+def imageToMap(in_path, out_path, filename):
+    image = Image.open(os.path.join(in_path, filename))
+    bg = Image.new('RGBA', (800, 800), color=(0, 0, 0, 0))
+    bg.paste(image, None, image)
+
+    newImage = bg.convert('LA')
+    pixdata = newImage.load()
+
+    width, height = newImage.size
+    for y in range(height):
+        for x in range(width):
+            if pixdata[x, y][1] == 0:
+                pixdata[x, y] = (1, 255)
+            elif pixdata[x, y][0] == 0:
+                pixdata[x, y] = (2, 255)
+            elif pixdata[x, y][0] == 255:
+                pixdata[x, y] = (3, 255)
+            else:
+                pixdata[x, y] = (4, 255)
+
+    newImage.save(out_path + filename)
---
+++ code_py/training.py
@@ -0,0 +1,128 @@
+import argparse
+import os
+from os.path import exists
+
+import numpy as np
+from PIL import Image
+from PIL.ImageOps import autocontrast
+from keras.utils import load_img
+from tensorflow import keras
+
+import filter
+import preprocessing
+from UNet import make_U_model
+
+
+# Configure the model for training.
+# We use the "sparse" version of categorical-crossentropy because our target data is integers.
+def train(model: keras.Model, train_gen: preprocessing.DataGen, val_gen: preprocessing.DataGen,
+          epochs: int = 15) -> None:
+    model.compile(optimizer=keras.optimizers.Adam(learning_rate=1e-4, clipnorm=0.001), loss="sparse_categorical_crossentropy",
+                  metrics=[keras.metrics.SparseCategoricalCrossentropy(name="no_bg", ignore_class=0)])
+
+    callbacks = [
+        keras.callbacks.TensorBoard(log_dir="tensorboard"),
+        keras.callbacks.EarlyStopping(monitor='no_bg',
+                                      min_delta=0.001,
+                                      patience=5,
+                                      mode='min',
+                                      verbose=1),
+        keras.callbacks.ModelCheckpoint("segmentation.h5", save_best_only=True),
+        keras.callbacks.ReduceLROnPlateau(
+            monitor='loss',
+            factor=0.1,
+            patience=3,
+            verbose=1,
+            mode='min',
+            min_delta=0.01,
+            cooldown=0,
+            min_lr=0
+        )
+    ]
+
+    # Train the model, doing validation at the end of each epoch.
+    model.fit(train_gen, epochs=epochs, validation_data=val_gen, callbacks=callbacks)
+
+
+def display_mask(masks, original, save_dir):
+    """Quick utility to display a model's prediction.
+    :param original: input image
+    :param masks: predictions
+    :param save_dir: comparison file path
+    """
+    for i in range(len(masks)):
+        mask = masks[i]
+        mask = np.argmax(mask, axis=-1)
+        mask = np.expand_dims(mask, axis=-1)
+        img = autocontrast(keras.preprocessing.image.array_to_img(mask)).convert('RGBA')
+        og = load_img(original[0][i], color_mode='rgba')
+        og_quad = autocontrast(load_img(original[1][i], color_mode='grayscale')).convert('RGBA')
+        bg = Image.new('RGBA', (2400, 800), color=(0, 0, 0, 0))
+        bg.paste(og, (0, 0))
+        bg.paste(og_quad, (800, 0))
+        bg.paste(img, (1600, 0))
+        bg.save(save_dir + str(i) + ".png")
+
+
+def init(args):
+    input_dir = args.input
+    target_dir = args.target
+    out_dir = './images/result/'
+    img_size = (800, 800)
+    num_classes = 4
+    batch_size = 4
+    input_img_paths, target_img_paths = filter.load_dataset_images(input_dir, target_dir)
+
+    print("Number of samples:", len(input_img_paths))
+
+    # Free up RAM in case the model definition cells were run multiple times
+    keras.backend.clear_session()
+
+    # Build model
+    model = make_U_model(img_size, num_classes)
+    if exists("./segmentation.h5"):
+        model.load_weights("./segmentation.h5")
+
+    train_gen, val_gen, val_paths = preprocessing.makeGens(input_img_paths, target_img_paths, batch_size, img_size, aug=args.train)
+    if args.train:
+        model.summary()
+        train(model, train_gen, val_gen, epochs=args.epochs)
+
+    val_preds = model.predict(val_gen)
+
+    os.makedirs(out_dir, exist_ok=True)
+    # Display mask predicted by our model
+    display_mask(val_preds, val_paths, out_dir)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--train",
+        type=bool,
+        metavar="train",
+        default=False,
+        help="False for inference, True for train. Default to false",
+    )
+    parser.add_argument(
+        "--input",
+        type=str,
+        metavar="input",
+        default="./images/input/",
+        help="Path of the input images",
+    )
+    parser.add_argument(
+        "--target",
+        type=str,
+        metavar="target",
+        default="./images/maps/",
+        help="Path of the annotation images",
+    )
+    parser.add_argument(
+        "--epochs",
+        type=int,
+        metavar="epochs",
+        default=15,
+        help="Number of epochs",
+    )
+    init(parser.parse_args())
